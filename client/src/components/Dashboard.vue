e3b9c2be63ac58afeb5e1f219f460dc7e1c708fd
anggraito <anggraito.laras@gmail.com>
2017-10-05 22:24:26 +0700

    dashboard popup done

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -13,15 +13,15 @@
               <small>Angka yang kamu pilih: </small>
               <h2>5</h2>
             </div>
+            <h3>Pilih Angka: </h3>
             <div class="modal-footer">
-              <button type="button" class="btn btn-default" value="1">1</button>
-              <button type="button" class="btn btn-default" value="2">2</button>
-              <button type="button" class="btn btn-default" value="3">3</button>
-              <button type="button" class="btn btn-default" value="4">4</button>
-              <button type="button" class="btn btn-default" value="5">5</button>
+              <button type="button" class="btn" value="1">1</button>
+              <button type="button" class="btn" value="2">2</button>
+              <button type="button" class="btn" value="3">3</button>
+              <button type="button" class="btn" value="4">4</button>
+              <button type="button" class="btn" value="5">5</button>
             </div>
-            <button type="button" class="btn btn-default" data-dismiss="modal">Submit</button>
-            
+            <button type="button" class="start-btn" data-dismiss="modal">Submit</button>
           </div><!-- /.modal-content -->
         </div><!-- /.modal-dialog -->
       </div><!-- /.modal -->
@@ -50,6 +50,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.modal{
+  text-align: center;
+}
+.modal-content{
+  padding: 10px;
+}
 .board{
   height: 100vh;
 }
@@ -73,5 +79,28 @@ export default {
   position: absolute;
   bottom: 0;
   width: 100%;
+}
+.modal-footer{
+  display: flex;
+  justify-content: center;
+}
+.modal-footer .btn:hover{
+  background-color: #febb32;
+  color: #fff;
+}
+.modal-footer .btn{
+  width: 100px;
+  height: 100px;
+}
+.modal-content .start-btn{
+  border-radius: 50px;
+  background-color: #febb32;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 700;
+  padding: 15px 35px;
+  height: 10%;
+  width: 100%;
+  margin: 20px 0;
 }
 </style>
