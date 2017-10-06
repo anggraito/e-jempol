bc282e9860e968df874c20c88f7c86513cfe92db
Yosua1011 <yosua1011@gmail.com>
2017-10-06 13:20:26 +0700

    Using http instead https on main.js baseURL

--- client/src/main.js
+++ client/src/main.js
@@ -25,7 +25,7 @@ const config = {
 const firebaseApp = firebase.initializeApp(config)
 
 Vue.prototype.$http = axios.create({
-  baseURL: `https://35.198.242.87/`
+  baseURL: `http://35.198.242.87/`
   // baseURL: `http://localhost:3000`
 })
 Vue.prototype.$db = firebaseApp.database()
